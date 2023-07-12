0b18ba665e4b5ad86f40eae8c0738b3aa0c2ced1
Alexandre Marcelo <contact@xandecelo.com>
2023-07-12 11:58:24 -0300

    Remove automatic load of dyngroups

--- run.sh
+++ run.sh
@@ -9,8 +9,9 @@ export PALDAP_ADMIN_CONFIG_USER="${PALDAP_ADMIN_CONFIG_USER:-cn=config}"
 export PALDAP_ADMIN_CONFIG_PASSWORD="${PALDAP_ADMIN_CONFIG_PASSWORD:-configadminpassword}"
 export PALDAP_ADMIN_USER="${PALDAP_ADMIN_USER:-cn=admin,${PALDAP_DEFAULT_BASE_DN}}"
 export PALDAP_ADMIN_PASSWORD="${PALDAP_ADMIN_PASSWORD:-adminpassword}"
-export PALDAP_LOAD_EXTRA_MODULES="${PALDAP_LOAD_EXTRA_MODULES:dyngroup}"
-export PALDAP_LOG_LEVEL="${PALDAP_LOG_LEVEL:=conns}"
+#export PALDAP_LOAD_EXTRA_MODULES="${PALDAP_LOAD_EXTRA_MODULES:dyngroup}"
+export PALDAP_LOAD_EXTRA_MODULES="${PALDAP_LOAD_EXTRA_MODULES:-}"
+export PALDAP_LOG_LEVEL="${PALDAP_LOG_LEVEL:=stats2}"
 
 end_padl_app() {
     echo "Shutting down ldap..."
@@ -25,6 +26,6 @@ then
     sudo /usr/bin/killall slapd
 fi
 echo "Restarting ldap service..."
-sudo /usr/sbin/slapd -h "ldap:/// ldapi:///" -g openldap -u openldap -F /opt/openldap/data -d  ${PALDAP_LOG_LEVEL}
+sudo /usr/sbin/slapd -h "ldap:/// ldapi:///" -g openldap -u openldap -F /opt/openldap/data -d ${PALDAP_LOG_LEVEL}
 
 # debug levels: slapd -d ?
